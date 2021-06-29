5fb061a57e168cbdd1506f871fcfd978dc8929af
vadimcvirko <promiti90@gmail.com>
2021-06-29 11:23:41 +0300

    fix header , footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
                     <li>
                         <a
-                            href='https://github.com'
+                            href='https://github.com/vadimlelik/shop'
                             target='_blank'
                             rel='noreferrer'
                         >
